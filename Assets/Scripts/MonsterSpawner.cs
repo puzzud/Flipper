f5a72599c7ed1a1586230f51f72134f4629f4adf
puzzud <puzzud@gmail.com>
2016-01-30 14:47:41 -0500

    #1 Monsters spawn over time.

---
+++ Assets/Scripts/MonsterSpawner.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Diagnostics;
+
+public class MonsterSpawner : MonoBehaviour {
+
+  public float minInterval = 2.0f;
+  public float maxInterval = 12.0f;
+
+  Stopwatch stopWatch = new Stopwatch();
+
+  double nextSpawnTime = 0.0f;
+
+  public GameObject monsterPrefab;
+  public GameObject[] spawnPoints;
+
+  void Awake()
+  {
+    
+  }
+
+	// Use this for initialization
+	void Start () {
+    stopWatch.Start();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    // TODO: Maybe rather than checking the time a lot,
+    // a time event mechanism can be used.
+    if (stopWatch.Elapsed.TotalSeconds >= nextSpawnTime)
+    {
+      spawnMonster();
+    }
+	}
+
+  double setNextSpawnTime()
+  {
+    nextSpawnTime = stopWatch.Elapsed.TotalSeconds + Random.Range(minInterval, maxInterval);
+    return nextSpawnTime;
+  }
+
+  GameObject getRandomSpawnPoint()
+  {
+    if (spawnPoints.Length < 1)
+    {
+      return null;
+    }
+
+    return spawnPoints[Random.Range(0, spawnPoints.Length)];
+  }
+
+  bool spawnMonster()
+  {
+    GameObject spawnPoint = getRandomSpawnPoint();
+    if (spawnPoint == null)
+    {
+      return false;
+    }
+    
+    Instantiate(monsterPrefab, spawnPoint.transform.position, spawnPoint.transform.rotation);
+    
+    setNextSpawnTime();
+
+    return true;
+  }
+}
