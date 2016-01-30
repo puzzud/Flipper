a3fc05d247e5cf7fd27712e8753984d9423c5aac
puzzud <puzzud@gmail.com>
2016-01-30 10:38:25 -0500

    Added ground plane and hero cube with WASD & cursor control.

---
+++ Assets/Scripts/HeroControl.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeroControl : MonoBehaviour {
+
+  CharacterController controller;
+  public float speed = 5.0f;
+  Vector3 movement = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+    controller = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    movement.x = Input.GetAxis("Horizontal") * speed;
+    movement.z = Input.GetAxis("Vertical") * speed;
+
+    controller.Move(movement * Time.deltaTime);
+	}
+}
