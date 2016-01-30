c3717f1f67d10a8bf11230c839aaa0c0c5d329e2
Tom Hrabchak <thomas.hrabchak@siemens.com>
2016-01-30 13:10:10 -0500

    #3 Add mouse rotation to hero

--- Assets/Scripts/HeroControl.cs
+++ Assets/Scripts/HeroControl.cs
@@ -5,7 +5,9 @@ public class HeroControl : MonoBehaviour {
 
   CharacterController controller;
   public float speed = 5.0f;
+  public Vector3 forwardDir;
   Vector3 movement = Vector3.zero;
+
 
 	// Use this for initialization
 	void Start () {
@@ -14,6 +16,14 @@ public class HeroControl : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+    // Hero Rotation (based on mouse positioning)
+    Vector3 pos = Camera.main.WorldToScreenPoint(transform.position);
+    Vector3 dir = Input.mousePosition - pos;
+    float angle = Mathf.Atan2(dir.x, dir.y) * Mathf.Rad2Deg;
+    transform.rotation = Quaternion.AngleAxis(angle, forwardDir); 
+
+    // Hero Movement
     movement.x = Input.GetAxis("Horizontal") * speed;
     movement.z = Input.GetAxis("Vertical") * speed;
 
