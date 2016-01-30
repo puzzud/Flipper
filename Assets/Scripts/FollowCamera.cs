e528b2c1abadc7c6f04abb0bf3474724686cfa56
Tom Hrabchak <thomas.hrabchak@siemens.com>
2016-01-30 16:01:09 -0500

    Add model outlines to scene

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCamera : MonoBehaviour {
+
+  public GameObject objectToFollow;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame (use LateUpdate for cameras)
+	void LateUpdate () {
+    transform.LookAt(objectToFollow.transform);
+	}
+}
