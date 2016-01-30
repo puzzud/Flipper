33fa6ed5dd1def277613f9dcf4f721cc1462b5e0
Tom Hrabchak <thomas.hrabchak@siemens.com>
2016-01-30 15:01:23 -0500

    Add script for switching between cameras

---
+++ Assets/Scripts/cameraController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraController : MonoBehaviour {
+
+  public Camera cam1;
+  public Camera cam2;
+
+	// Use this for initialization
+	void Start () {
+    cam1.enabled = true;
+    cam2.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    if (Input.GetKeyDown(KeyCode.C))
+    {
+      cam1.enabled = !cam1.enabled;
+      cam2.enabled = !cam2.enabled;
+    }
+	}
+}
