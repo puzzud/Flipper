425515f2feb36a139ecb80271dbab14d8f82697c
Tom Hrabchak <thomas.hrabchak@siemens.com>
2016-01-30 22:41:54 -0500

    #5 when projectiles collide with box monsters, both are destroyed

--- Assets/Scripts/ProjectileMovement.cs
+++ Assets/Scripts/ProjectileMovement.cs
@@ -8,12 +8,21 @@ public class ProjectileMovement : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-
 	}
 	
 	// Update is called once per frame
 	void Update () {
-    transform.position = Vector3.MoveTowards(transform.position, movementDirection + transform.position, speed * Time.deltaTime);
-    
+    transform.position = Vector3.MoveTowards(transform.position, movementDirection + transform.position, speed * Time.deltaTime);    
 	}
+
+  void OnTriggerEnter(Collider other)
+  {
+    if (other.name == "Box Monster(Clone)")
+    {
+      Debug.Log("Box HIT!!!");
+      Destroy(other.gameObject);
+      Destroy(this.gameObject);
+    }
+  }
+
 }
